9ab27f246c976df1b37162539becc076d98a8c56
Tatsuro <contacts@tatsuro.dev>
2022-01-24 15:03:24 +0900

    styleをまとめる 22

--- news-app/App.js
+++ news-app/App.js
@@ -1,21 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View
-        style={{
-          height: 100,
-          width: '100%',
-          borderColor: 'gray',
-          borderWidth: 1,
-        }}
-      ></View>
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -23,4 +8,18 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.itemContainer}></View>
+    </View>
+  );
+}
