413497ac27590446ad9ceddda82b377ed92a7ad3
Tatsuro <contacts@tatsuro.dev>
2022-01-25 13:45:12 +0900

    clip系actionをdispatch 61

--- news-app/screens/ArticleScreen.js
+++ news-app/screens/ArticleScreen.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { StyleSheet, SafeAreaView, Text, TouchableOpacity } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { useDispatch } from 'react-redux';
+import { addClip, deleteClip } from '../store/actions/user';
 
 const styles = StyleSheet.create({
   container: {
@@ -12,9 +14,24 @@ const styles = StyleSheet.create({
 export default function ArticleScreen({ route }) {
   // route.paramsで渡されたparameterを取得
   const { article } = route.params;
+  const dispatch = useDispatch();
 
   return (
     <SafeAreaView style={styles.container}>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(addClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>ADD_CLIP</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        onPress={() => {
+          dispatch(deleteClip({ clip: article }));
+        }}
+      >
+        <Text style={{ margin: 10, fontSize: 30 }}>DELETE_CLIP</Text>
+      </TouchableOpacity>
       <WebView source={{ uri: article.url }} />
     </SafeAreaView>
   );
