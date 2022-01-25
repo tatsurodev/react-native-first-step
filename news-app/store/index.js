041b60fffc5d54bf699e5a085c1e052d5d3ed98c
Tatsuro <contacts@tatsuro.dev>
2022-01-25 13:33:13 +0900

    reduxのdebugger install, setup 60

--- news-app/store/index.js
+++ news-app/store/index.js
@@ -1,10 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import userReducer from './reducers/user';
+// reduxのdebuggingに使用
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, composeWithDevTools());
 
 export default store;
