67685cfa0452bdb763700d9fb70678977e304b08
Tatsuro <contacts@tatsuro.dev>
2022-02-01 05:33:45 +0900

    reduxを永続化 67

--- news-app/store/index.js
+++ news-app/store/index.js
@@ -2,11 +2,22 @@ import { createStore, combineReducers } from 'redux';
 import userReducer from './reducers/user';
 // reduxのdebuggingに使用
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const rootReducer = combineReducers({
   user: userReducer,
 });
 
-const store = createStore(rootReducer, composeWithDevTools());
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage,
+  whitelist: ['user'],
+};
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = createStore(persistedReducer, composeWithDevTools());
+
+export const persistor = persistStore(store);
 export default store;
