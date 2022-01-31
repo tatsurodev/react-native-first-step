11d9bb357c80c07f2cf30935e9e932b59401d0ec
Tatsuro <contacts@tatsuro.dev>
2022-02-01 04:40:30 +0900

    clipsをstateから取得、表示 64

--- news-app/screens/ClipScreen.js
+++ news-app/screens/ClipScreen.js
@@ -1,4 +1,6 @@
-import { StyleSheet, SafeAreaView, Text } from 'react-native';
+import { useSelector } from 'react-redux';
+import { StyleSheet, SafeAreaView, Text, FlatList } from 'react-native';
+import ListItem from '../components/ListItem';
 
 const styles = StyleSheet.create({
   container: {
@@ -7,10 +9,23 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function ClipScreen() {
+export default function ClipScreen({ navigation }) {
+  const user = useSelector((state) => state.user);
+  const { clips } = user;
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Clip Screen</Text>
+      <FlatList
+        data={clips}
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+            onPress={() => navigation.navigate('Article', { article: item })}
+          />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+      />
     </SafeAreaView>
   );
 }
